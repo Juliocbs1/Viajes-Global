bb822bf22f0e542e751219dd55ef6f375a2b0593
Juliocbs1 <barriossanchezjuliocesar86@gmail.com>
2024-09-15 11:17:14 -0500

    Proyecto

---
+++ src/main/java/com/viajesglobal/ViajesglobalApplication.java
@@ -0,0 +1,30 @@
+package com.viajesglobal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ViajesglobalApplication {
+
+    /*echo "# Viajes-Global" >> README.md
+    git init
+    git add README.md
+    git commit -m "first commit"
+    git branch -M main
+    git remote add origin https://github.com/Juliocbs1/Viajes-Global.git
+    git push -u origin main
+    */
+
+    @GetMapping
+    public String welcome() {
+        return "Welcome to Viajesglobal";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViajesglobalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/viajesglobal/ViajesglobalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viajesglobal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViajesglobalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
