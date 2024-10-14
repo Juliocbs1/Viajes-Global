7d4e1a8059a8f6f8b6d0a89636467369730d0890
Juliocbs1 <barriossanchezjuliocesar86@gmail.com>
2024-10-14 13:40:22 -0500

    Creacion entidad paquetes

--- src/main/java/com/viajesglobal/entity/Paquetes.java
+++ src/main/java/com/viajesglobal/entity/Paquetes.java
@@ -1,4 +1,28 @@
 package com.viajesglobal.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Paquetes")
 public class Paquetes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_paquete")
+    private int idPaquete;
+
+    private String nombre;
+    private String descripcion;
+    private long precio;
+    private int disponibilidad;
+    @Column(name = "id_destino")
+    private int idDestino;
+
+
+
 }
